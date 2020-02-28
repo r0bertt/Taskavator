6ff0b5c333e1cac0950c8a69ad6dbca03c7cdae3
r0bertt <lazauskas.rob@gmail.com>
2020-02-29 01:04:04 +0200

    edited if statements and added a regex to the year fields

--- web/modules/custom/task_copyright/src/Plugin/Block/TaskCopyright.php
+++ web/modules/custom/task_copyright/src/Plugin/Block/TaskCopyright.php
@@ -64,6 +64,20 @@ class TaskCopyright extends BlockBase {
   /**
    * {@inheritdoc}
    */
+  public function blockValidate($form, FormStateInterface $form_state) {
+    parent::blockValidate($form, $form_state);
+
+    if (!(preg_match("^(19[0-8][0-9]|199[0-9]|20[0-8][0-9]|209[0-9])$", $this->configuration['year_origin']))) {
+      $form_state->setError($form['year_origin'], $this->t('Please edit your origin date'));
+    }
+    if (!(preg_match("^(19[0-8][0-9]|199[0-9]|20[0-8][0-9]|209[0-9])$", $this->configuration['year_to_date']))) {
+      $form_state->setError($form['year_to_date'], $this->t('Please edit your year to date'));
+    }
+  }
+
+  /**
+   * {@inheritdoc}
+   */
   public function blockSubmit($form, FormStateInterface $form_state) {
     $this->configuration['organization_name'] = $form_state->getValue('organization_name');
     $this->configuration['year_origin'] = $form_state->getValue('year_origin');
@@ -77,26 +91,34 @@ class TaskCopyright extends BlockBase {
     $date = new \DateTime();
 
     // From $year_to_date to Present.
-    $year_to_date = empty($this->configuration['year_to_date'])
-      ? $date->format('Y')
-      : $this->configuration['year_to_date'];
+    if ($year_to_date = empty($this->configuration['year_to_date'])) {
+      $date->format('Y');
+    }
+    else {
+      $this->configuration['year_to_date'];
+    }
 
-    return $this->configuration['year_origin'] == 0 || $date->format('Y') == $this->configuration['year_origin']
-      ? [
-        '#type' => 'markup',
-        '#markup' => $this->t('Copyright &copy; @year @organization', [
-          '@year' => $date->format('Y'),
-          '@organization' => $this->configuration['organization_name'],
-        ]),
-      ]
-      : [
-        '#type' => 'markup',
-        '#markup' => $this->t('Copyright &copy; @year_origin-@year_to_date @organization', [
-          '@year_origin' => $this->configuration['year_origin'],
-          '@year_to_date' => $year_to_date,
-          '@organization' => $this->configuration['organization_name'],
-        ]),
-      ];
+    if ($this->configuration['year_origin'] == 0 || $date->format('Y') == $this->configuration['year_origin']) {
+      return
+        [
+          '#type' => 'markup',
+          '#markup' => $this->t('Copyright &copy; @year @organization', [
+            '@year' => $date->format('Y'),
+            '@organization' => $this->configuration['organization_name'],
+          ]),
+        ];
+    }
+    else {
+      return
+        [
+          '#type' => 'markup',
+          '#markup' => $this->t('Copyright &copy; @year_origin-@year_to_date @organization', [
+            '@year_origin' => $this->configuration['year_origin'],
+            '@year_to_date' => $year_to_date,
+            '@organization' => $this->configuration['organization_name'],
+          ]),
+        ];
+    }
   }
 
 }
